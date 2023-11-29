85172457c79e7053a7a1e5e3926827bb45818d87
Geoffrey Tsui <tsui.geoffrey@gmail.com>
2023-11-29 17:26:44 +0800

    update particle

--- src/providers/WagmiProvider.tsx
+++ src/providers/WagmiProvider.tsx
@@ -20,21 +20,11 @@ const WagmiProvider = ({ children }: { children: React.ReactNode }) => {
     [publicProvider()]
   );
 
-  const particle = useMemo(() => {
-    try {
-      console.log("Its working");
-      new ParticleNetwork({
-        projectId: "5cb4fb13-1396-4e8f-b524-9e062f81245d",
-        clientKey: "c6qP1SPPdvxqI4EKLSJPpPJfj8RacOmVDZ0SFQYB",
-        appId: "e808e5ef-caa5-4847-8dc6-7b416ecfaf42",
-        chainName: "Ethereum",
-        chainId: 1,
-        wallet: { displayWalletEntry: false },
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  }, []);
+  const particle = new ParticleNetwork({
+    projectId: "5cb4fb13-1396-4e8f-b524-9e062f81245d",
+    clientKey: "c6qP1SPPdvxqI4EKLSJPpPJfj8RacOmVDZ0SFQYB",
+    appId: "e808e5ef-caa5-4847-8dc6-7b416ecfaf42",
+  });
 
   //const supportedWallets = useMemo(
   //  () => ({
